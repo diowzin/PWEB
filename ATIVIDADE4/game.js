221c3fa5b88ca5ebf137166e5cca0b526897eb7c
diowzin <jonatasconrado10@gmail.com>
2020-05-13 21:35:35 -0300

    Add files via upload

---
+++ ATIVIDADE4/game.js
@@ -0,0 +1,39 @@
+        var jogador=prompt("Digite a opção: " + "\n" + "Pedra | Papel | Tesoura");
+        
+
+        if ((jogador != "Pedra") && (jogador != "Papel") && (jogador != "Tesoura"))
+            {
+            alert("OPÇÃO INVÁLIDA");
+            }else {
+
+
+        var min = Math.ceil(min);
+        var max = Math.floor(max);
+    
+        var resultado = Math.floor(Math.random() * 3);
+
+        if(jogador == "Pedra" && resultado == 0){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Empate");
+        }else if (jogador == "Papel" && resultado == 0){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Papel cobriu Pedra");
+        }else if (jogador == "Tesoura" && resultado == 0){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Pedra quebrou tesoura");
+        }
+            
+        
+        if(jogador == "Pedra" && resultado == 1){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Papel cobriu Pedra");
+        }else if (jogador == "Papel" && resultado == 1){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Empate");
+        }else if (jogador == "Tesoura" && resultado == 1){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Tesoura cortou papel");
+        }
+
+        if(jogador == "Pedra" && resultado == 2){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Pedra quebrou tesoura");
+        }else if (jogador == "Papel" && resultado == 2){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Tesoura cortou papel");
+        }else if (jogador == "Tesoura" && resultado == 2){
+            alert("Você é " + jogador + "\n" + "RESULTADO: " + "Empate");
+        }
+    }
